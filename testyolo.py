dd3d4b461f3b14d692c12d5463942d787e922f1c
hokuseihal <hokuseihal@gmail.com>
2020-01-20 22:34:25 +0900

    fix

--- testyolo.py
+++ testyolo.py
@@ -4,6 +4,6 @@ from cal_score5 import precision_recall
 backuppath='/home/wadanaoki/src/mydarknet/backup'
 
 for w in os.listdir(backuppath):
-    call=['./darknet','rdd','cg/yolov2-rdd.cfg',w,'test.txt']
+    call=['/home/wadanaoki/src/mydarknet/darknet','rdd','cg/yolov2-rdd.cfg',w,'test.txt']
     subprocess.call(call)
     precision_recall(csvfilename='result.csv')
